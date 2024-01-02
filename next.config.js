4294b50759c42a059fb8c41fd6b37af565062d96
Tejas Kulkarni <tejaskulkarni294@gmail.com>
2024-01-02 07:17:57 +0530

    fixed image not rendering

--- next.config.js
+++ next.config.js
@@ -3,7 +3,17 @@ const nextConfig = {
     poweredByHeader: true,
     productionBrowserSourceMaps: true,
     compress: true,
-    cleanDistDir: true
+    cleanDistDir: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'mynewfoliobucket.s3.ap-south-1.amazonaws.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    }
 }
 
 module.exports = nextConfig
